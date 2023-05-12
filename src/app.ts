02563cdebe68d4eb44fd89aca6299d5ce2e8d27d
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-05-11 19:28:53 -0700

    more variations of arrow functions

--- src/app.ts
+++ src/app.ts
@@ -20,5 +20,16 @@ age = 29;
 // console.log(result);
 
 const add = (a: number, b: number) => a + b;
+// Only works if there's one expression
 
 console.log(add(2, 5));
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector('button');
+
+if (button) {
+  button.addEventListener('click', event => console.log(event));
+}
+
+printOutput(add(5, 2));
