c9ae6ce999bccafe87a3498e32c86eee9d8d6d34
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-05-14 09:14:20 -0700

    example of rest params and tuples

--- src/app.ts
+++ src/app.ts
@@ -46,13 +46,13 @@ const person = {
 
 const copiedPerson = {...person};
 
-const add = (...numbers: number[]) => {
+const add = (...numbers: [number, number, number]) => {
 
   return numbers.reduce((curResult, curValue) => {
     return curResult + curValue;
   }, 0);
 };
 
-const addedNumbers = add(5, 10, 2, 3.7);
+const addedNumbers = add(5, 10, 2);
 
 console.log(addedNumbers);
