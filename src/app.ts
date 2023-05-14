8bb770ff0d89b00e5a0d82e77fd14b46b1f2b4fe
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-05-14 08:50:26 -0700

    added default for example

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ age = 29;
 
 // console.log(result);
 
-const add = (a: number, b: number) => a + b;
+const add = (a: number, b: number = 1) => a + b;
 // Only works if there's one expression
 
 console.log(add(2, 5));
@@ -32,4 +32,4 @@ if (button) {
   button.addEventListener('click', event => console.log(event));
 }
 
-printOutput(add(5, 2));
+printOutput(add(5, 3));
